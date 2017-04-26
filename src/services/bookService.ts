9ac34297de7633f0a3f099365fb009d30c0a0379
carles <devlitus>
2017-04-26 21:48:01 +0200

    modificar

---
+++ src/services/bookService.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Jsonp, Headers } from "@angular/http";
+import "rxjs/add/operator/map";
+
+@Injectable()
+export class BookService {
+
+    constructor(private _service: Jsonp) { }
+
+    
+}
